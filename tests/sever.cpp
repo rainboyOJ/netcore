8cd1d7c6fe144e146f0a07b74e24925b8f3c55a6
rainboy <rainboylvx@qq.com>
2022-01-14 09:35:10 +0800

    加入 登录测试

--- tests/sever.cpp
+++ tests/sever.cpp
@@ -1,10 +1,22 @@
 #include <regex>
 #include "http_server.hpp"
 
+using namespace rojcpp;
+struct check_login {
+    bool before(request& req,response& rep){
+        auto weak_session = req.get_session();
+        if( auto session = weak_session.lock(); session != nullptr){
+            auto isLogin = session->get_data<bool>("isLogin");
+            if( isLogin ) return true;
+        }
+        rep.set_status_and_content(status_type::ok,"your are not logined!",req_content_type::string);
+        return false;
+    }
+};
+
 //创建一个全局的服务器
 rojcpp::http_server_ Server;
 int main(){
-    using namespace rojcpp;
     Server.set_http_handler<GET>("/helloworld",[](request & req,response & res){
             res.set_status_and_content(status_type::ok,"hello world,this rojcpp Server",req_content_type::string);
     });
@@ -20,8 +32,41 @@ int main(){
     Server.set_http_regex_handler<GET>(regex_route, [](request & req,response & res){
             res.set_status_and_content(status_type::ok,"hello world,regex_route",req_content_type::string);
         });
-        
+    
+    Server.set_http_handler<GET>("/msg_after_logined",
+            [](request & req,response & res){
+                res.set_status_and_content(status_type::ok,"yes! you logined! This secret msg!",req_content_type::string);
+            } , check_login{});
 
+    Server.set_http_handler<POST>("/login",
+            [](request & req,response & res){
+                auto weak_session = req.get_session();
+                if( auto session = weak_session.lock(); session != nullptr){
+                    auto isLogin = session->get_data<bool>("isLogin");
+                    if( isLogin ) {
+                        res.set_status_and_content(status_type::ok,"you have logined!",req_content_type::string);
+                        return ;
+                    }
+                }
+                auto session = res.start_session();
+                session->set_data("isLogin", true);
+                res.set_status_and_content(status_type::ok,"yes,now logined",req_content_type::string);
+            });
+
+    Server.set_http_handler<POST>("/signout",
+            [](request & req,response & res){
+                auto weak_session = req.get_session();
+                if( auto session = weak_session.lock(); session != nullptr){
+                    auto isLogin = session->get_data<bool>("isLogin");
+                    if( isLogin ) {
+                        res.set_status_and_content(status_type::ok,"you have logined!",req_content_type::string);
+                        return ;
+                    }
+                }
+                auto session = res.start_session();
+                session->set_data("isLogin", true);
+                res.set_status_and_content(status_type::ok,"yes,now logined",req_content_type::string);
+            });
 
 
 
