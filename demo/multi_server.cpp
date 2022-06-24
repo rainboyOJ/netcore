f60bb0d382728de9ea07cea9fe871a6ef3999a92
rainboy <rainboylvx@qq.com>
2022-06-24 14:09:16 +0800

    url_query 测试

--- demo/multi_server.cpp
+++ demo/multi_server.cpp
@@ -15,10 +15,57 @@ int main(){
     LOG(INFO) << "hello word info \n" ;
     LOG(DEBUG) << "hello word info \n" ;
 
-    http_server hs(2,8899);
-    hs.set_http_handler<GET>("/hello_word", [](request & req, response &res){
+    http_server hs(1,8899);
+    hs.set_http_handler<GET>("/hello", [](request & req, response &res){
             res.set_status_and_content(status_type::ok, "hello world");
             });
+
+
+    hs.regist_ws_conn_check("/ws", [](request & req,response & res){
+            return true;
+    });
+
+    //websocket 功能
+    // 处理过程 1.upgrade , 2 处理
+    hs.set_http_handler<GET>("/ws",[](request & req,response & res){
+
+            req.on(ws_open, [](request &req){
+                    std::cout << "ws_open" << std::endl;
+            });
+
+            req.on(ws_close, [](request &req){
+                    std::cout << "ws_close" << std::endl;
+            });
+
+
+            req.on(ws_message, [&res](request &req){
+                    std::cout << "ws_message" << std::endl;
+                    auto part_view = req.get_part_data();
+                    std::cout << req.get_part_data() << std::endl;
+                    //返回信息
+                    //使用ws 创建一个信息
+                    //req.get_conn()->build_ws_string(part_view.data(), part_view.length());
+                    res.set_response(websocket::format_message(part_view.data(), part_view.length(), opcode::text));
+            });
+
+            req.on(ws_error, [](request &req){
+                    std::cout << "ws_error" << std::endl;
+            });
+
+        });
+
+    // url_query 处理
+    // ?value=100
+    hs.set_http_handler<GET>("/query",[](request & req,response & res){
+            std::cout << "/query, the query is" << std::endl;
+            int value = req.get_query_value<int>("value");
+            std::cout << "value : " << value << std::endl;
+            res.set_status_and_content(status_type::ok, std::to_string(value));
+    });
+    // 表单上传
+    // 文件上传
+    // 文件下载
+
     hs.run();
 
     return 0;
---
+++ tests/query_value.sh
@@ -0,0 +1,1 @@
+curl -v 'http://127.0.0.1:8899/query?value=100'
